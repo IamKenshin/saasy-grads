d14a70e45c293af8e4c0743a160087c8439887cc
kkorolyov <kirillk753@gmail.com>
2017-05-01 00:29:50 -0700

    Finish grad component?

--- grad/grad.component.js
+++ grad/grad.component.js
@@ -13,6 +13,23 @@ angular
 				this.years = []
 				for (let i = -2; i <= 2; i++) this.years.push(Number(this.user.year) + i)
 
+				this.passedAll = function () {
+					let passedAll = true
+
+					for (let i = 0; i < this.user.courses.length; i++) {
+						if (!this.passed(this.user.courses[i])) {
+							passedAll = false
+							break
+						}
+					}
+					return passedAll
+				}
+				this.passed = function (course) {
+					return ['A+', 'A', 'A-',
+									'B+', 'B', 'B-',
+									'C+', 'C', 'C-'].indexOf(course.grade) >= 0
+				}
+
 				console.log(this.user)
 			}
 		}]
